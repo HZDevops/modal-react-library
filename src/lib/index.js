c897eef8af4eddcc31746de5b659c142f377f79d
Zoubeiri <housna.zoubeiri@gmail.com>
2023-01-05 09:34:03 +0100

    adding index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,17 @@
-import Modal from "./components/Modal";
+"use strict";
 
-export { Modal };
+Object.defineProperty(exports, "__esModule", {
+  value: true,
+});
+Object.defineProperty(exports, "Modal", {
+  enumerable: true,
+  get: function get() {
+    return _Modal.default;
+  },
+});
+
+var _Modal = _interopRequireDefault(require("./components/Modal"));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
