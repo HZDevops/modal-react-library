aa5af62157dd59ebd09e8b79135947c7bc411798
Zoubeiri <housna.zoubeiri@gmail.com>
2023-01-05 10:08:04 +0100

    Cleaning code

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,17 +1,3 @@
-"use strict";
+import Modal from "./components/Modal";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true,
-});
-Object.defineProperty(exports, "Modal", {
-  enumerable: true,
-  get: function get() {
-    return _Modal.default;
-  },
-});
-
-var _Modal = _interopRequireDefault(require("./components/Modal"));
-
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
+export { Modal };
