a74e1886ccb66caa01484bb987ac0ff7353effd6
Zoubeiri <housna.zoubeiri@gmail.com>
2023-02-09 20:33:49 +0100

    CSS modifications

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Modal />
+      <Modal content="hello" buttonStyle={{ backgroundColor: "red" }} />
     </div>
   );
 }
